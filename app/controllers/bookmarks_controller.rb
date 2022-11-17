d2f938751aefaae7c57089cb5c01112217b24ac7
Corinne Porée <corinne.poree@gmail.com>
2022-11-17 14:49:37 +0100

    le controller list et actions et vue

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,13 @@
+class BookmarksController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/create'
+  get 'bookmarks/new'
+  get 'bookmarks/show'
+  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists
 end
